d6c306f7dac6d30a50e708c21a95b3b6e3b59782
Musap Kahraman <musapkahraman@gmail.com>
2020-07-30 03:38:55 +0300

    Add cell labels printer editor script

---
+++ Battleship-Client/Assets/Scripts/UI/Editor/CellLabelPrinterEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace BattleshipGame.UI
+{
+    [CustomEditor(typeof(CellLabelPrinter))]
+    public class CellLabelPrinterEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+            var indexPrinter = (CellLabelPrinter) target;
+            if (GUILayout.Button("Print")) indexPrinter.Print();
+            if (GUILayout.Button("Undo")) indexPrinter.Undo();
+        }
+    }
+}
