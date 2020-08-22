f96c06483897358f9c1e774038819d692bdbd03d
Mustafa Tuncer <tuncerm@gmail.com>
2020-08-22 17:51:08 +0200

    Updated server code to accept quering rooms by name.

--- Battleship-Server/src/lobby-room.ts
+++ Battleship-Server/src/lobby-room.ts
@@ -6,8 +6,12 @@ import { LobbyState } from "./lobby-state";
 export class CustomLobbyRoom extends LobbyRoom {
     async onCreate(options) {
         await super.onCreate(options);
-        console.log(options);
         this.setState(new LobbyState());
+
+        this.onMessage("query", (client, message)=>{
+            const matched = this.rooms.filter(room => room.metadata.name == message.name);
+            client.send("query", matched[0] ? matched[0].roomId: null);
+        });
     }
 
     onJoin(client: Client, options) {
