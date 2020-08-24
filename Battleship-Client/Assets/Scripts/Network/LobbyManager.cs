7e70f8cd1246d19d2569343641acc81315c9b4ae
Musap Kahraman <musapkahraman@gmail.com>
2020-08-24 18:09:24 +0300

    Add basic setup for games list in the lobby

--- Battleship-Client/Assets/Scripts/Network/LobbyManager.cs
+++ Battleship-Client/Assets/Scripts/Network/LobbyManager.cs
@@ -1,13 +1,29 @@
-﻿using System;
+﻿using BattleshipGame.UI;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace BattleshipGame.Network
 {
     public class LobbyManager : MonoBehaviour
     {
+        [SerializeField] private RoomsListManager roomsList;
         private void Start()
         {
-            ConnectionManager.Instance.ConnectToServer();
+            if (ConnectionManager.TryGetInstance(out var connectionManager))
+            {
+                connectionManager.ConnectToServer();
+                PopulateRoomsList();
+            }
+            else
+            {
+                SceneManager.LoadScene(0);
+            }
+        }
+
+        private void PopulateRoomsList()
+        {
+            
+            roomsList.SetRooms(15);
         }
     }
 }
