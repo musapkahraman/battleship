ba4cb30b78f6cc3fa8e677e8b9331368703d7097
Musap Kahraman <musapkahraman@gmail.com>
2020-09-03 08:15:12 +0300

    Fix clear button not restoring the ships in the pool

---
+++ Battleship-Client/Assets/Scripts/TilePaint/Pool.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using BattleshipGame.UI;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace BattleshipGame.TilePaint
+{
+    public class Pool : MonoBehaviour
+    {
+        [SerializeField] private ButtonController clearButton;
+        [SerializeField] private Tilemap tilemap;
+        private readonly Dictionary<Vector3Int, TileBase> _cache = new Dictionary<Vector3Int, TileBase>();
+
+        private void Start()
+        {
+            clearButton.AddListener(ResetPlacementMap);
+            foreach (var coordinate in tilemap.cellBounds.allPositionsWithin)
+            {
+                if (!tilemap.HasTile(coordinate)) continue;
+                if (_cache.ContainsKey(coordinate))
+                    _cache[coordinate] = tilemap.GetTile(coordinate);
+                else
+                    _cache.Add(coordinate, tilemap.GetTile(coordinate));
+            }
+        }
+
+        private void ResetPlacementMap()
+        {
+            tilemap.ClearAllTiles();
+            foreach (var kvp in _cache) tilemap.SetTile(kvp.Key, kvp.Value);
+        }
+    }
+}
