71e1191fd7096e5d288200c98d7312dce11d5ce3
Musap Kahraman <musapkahraman@gmail.com>
2020-09-03 08:21:56 +0300

    Fix random button not clearing the ships in the pool

--- Battleship-Client/Assets/Scripts/TilePaint/Pool.cs
+++ Battleship-Client/Assets/Scripts/TilePaint/Pool.cs
@@ -8,12 +8,14 @@ namespace BattleshipGame.TilePaint
     public class Pool : MonoBehaviour
     {
         [SerializeField] private ButtonController clearButton;
+        [SerializeField] private ButtonController randomButton;
         [SerializeField] private Tilemap tilemap;
         private readonly Dictionary<Vector3Int, TileBase> _cache = new Dictionary<Vector3Int, TileBase>();
 
         private void Start()
         {
-            clearButton.AddListener(ResetPlacementMap);
+            clearButton.AddListener(ResetThePool);
+            randomButton.AddListener(ClearThePool);
             foreach (var coordinate in tilemap.cellBounds.allPositionsWithin)
             {
                 if (!tilemap.HasTile(coordinate)) continue;
@@ -24,9 +26,14 @@ namespace BattleshipGame.TilePaint
             }
         }
 
-        private void ResetPlacementMap()
+        private void ClearThePool()
         {
             tilemap.ClearAllTiles();
+        }
+
+        private void ResetThePool()
+        {
+            ClearThePool();
             foreach (var kvp in _cache) tilemap.SetTile(kvp.Key, kvp.Value);
         }
     }
