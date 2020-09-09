f5173b68a0f10e826e6181440297fc8ec910dcfa
Musap Kahraman <musapkahraman@gmail.com>
2020-09-09 05:03:23 +0300

    fix: Avoid duplicate scenes

--- Battleship-Client/Assets/Scripts/Core/ProjectScenesManager.cs
+++ Battleship-Client/Assets/Scripts/Core/ProjectScenesManager.cs
@@ -15,37 +15,48 @@ namespace BattleshipGame.Core
         {
             base.Awake();
             SceneManager.sceneLoaded += OnSceneLoaded;
-            SceneManager.sceneUnloaded += OnSceneUnloaded;
             if (SceneManager.sceneCount > 1) UnloadAllScenesExcept(masterScene);
         }
 
         protected override void OnDestroy()
         {
             SceneManager.sceneLoaded -= OnSceneLoaded;
-            SceneManager.sceneUnloaded -= OnSceneUnloaded;
             base.OnDestroy();
         }
 
         public void GoToLobby()
         {
-            LoadSingleSceneAdditive(lobbyScene);
+            if (!IsAlreadyLoaded(lobbyScene))
+                LoadSingleSceneAdditive(lobbyScene);
         }
 
         public void GoToPlanScene()
         {
-            LoadSingleSceneAdditive(planScene);
+            if (!IsAlreadyLoaded(planScene))
+                LoadSingleSceneAdditive(planScene);
         }
 
         public void GoToBattleScene()
         {
-            LoadSingleSceneAdditive(battleScene);
+            if (!IsAlreadyLoaded(battleScene))
+                LoadSingleSceneAdditive(battleScene);
         }
 
         private void LoadSingleSceneAdditive(SceneReference sceneReference)
         {
             UnloadAllScenesExcept(masterScene);
-            Debug.Log($"<color=yellow>Loading {sceneReference.ScenePath}</color>");
             SceneManager.LoadScene(sceneReference, LoadSceneMode.Additive);
+        }
+
+        private static bool IsAlreadyLoaded(SceneReference sceneReference)
+        {
+            for (var i = 0; i < SceneManager.sceneCount; i++)
+            {
+                var scene = SceneManager.GetSceneAt(i);
+                if (scene.path.Equals(sceneReference.ScenePath)) return true;
+            }
+
+            return false;
         }
 
         private static void UnloadAllScenesExcept(SceneReference sceneReference)
@@ -54,20 +65,13 @@ namespace BattleshipGame.Core
             {
                 var scene = SceneManager.GetSceneAt(i);
                 if (scene.path.Equals(sceneReference.ScenePath)) continue;
-                Debug.Log($"<color=yellow>Unloading {scene.name}</color>");
                 SceneManager.UnloadSceneAsync(scene);
             }
         }
 
         private static void OnSceneLoaded(Scene scene, LoadSceneMode mode)
         {
-            Debug.Log($"<color=green>{scene.name}</color> is loaded.");
             SceneManager.SetActiveScene(scene);
-        }
-
-        private static void OnSceneUnloaded(Scene scene)
-        {
-            Debug.Log($"<color=red>{scene.name}</color> is unloaded.");
         }
     }
 }
