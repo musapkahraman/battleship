31632539d42f897b44037822d3ccca549b1aa6bb
Musap Kahraman <musapkahraman@gmail.com>
2020-12-26 16:35:28 +0300

    feat: Add support for different screen ratios

---
+++ Battleship-Client/Assets/Scripts/UI/CameraProjectionResizer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace BattleshipGame.UI
+{
+    public class CameraProjectionResizer : MonoBehaviour
+    {
+        private const float ReferenceRatio = 16f / 9;
+
+        private void Awake()
+        {
+            float screenRatio = (float) Screen.width / Screen.height;
+            if (screenRatio < ReferenceRatio)
+            {
+                float multiplier = ReferenceRatio / screenRatio;
+                GetComponent<Camera>().orthographicSize *= multiplier;
+            }
+        }
+    }
+}
---
+++ Battleship-Client/Assets/Scripts/UI/SpriteResizer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace BattleshipGame.UI
+{
+    public class SpriteResizer : MonoBehaviour
+    {
+        private const float ReferenceRatio = 16f / 9;
+
+        private void Awake()
+        {
+            var t = transform;
+            float screenRatio = (float) Screen.width / Screen.height;
+            if (screenRatio < ReferenceRatio)
+            {
+                float multiplier = ReferenceRatio / screenRatio;
+                var scale = t.localScale;
+                scale.y *= multiplier;
+                t.localScale = scale;
+            }
+            else
+            {
+                float multiplier = screenRatio / ReferenceRatio;
+                var scale = t.localScale;
+                scale.x *= multiplier;
+                t.localScale = scale;
+            }
+        }
+    }
+}
